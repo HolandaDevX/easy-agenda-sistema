6c9af3c730183242e6b903f83366c8a66e4e3af7
HolandaDevx <holandaj950@gmail.com>
2026-08-09 15:04:02 -0300

    commit inicial

---
+++ scripts/bancos-usuarios.js
@@ -0,0 +1,17 @@
+const arrUsers = [
+    user01 = {
+        nome: 'Joel',
+        email: 'joelholanda123@gmail.com',
+        senha: 1234565
+    },
+    user02 = {
+        nome: 'Pedro',
+        email: 'pedrooliveira@gmail.com',
+        senha: 101213
+    },
+    user01 = {
+        nome: 'João',
+        email: 'joaosilva45@gmail.com',
+        senha: 10121314
+    },
+]
