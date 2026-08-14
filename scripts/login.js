2423b6909fbe21dd108df0f7e72326c793c5e2ac
HolandaDevx <holandaj950@gmail.com>
2026-08-13 21:44:31 -0300

    testando

--- scripts/login.js
+++ scripts/login.js
@@ -23,7 +23,7 @@ formLogin.addEventListener('submit', (e) => {
     })
 
     if (autenticar) {
-        window.location = "app/appointment.html"
+        window.location = "/app/appointment.html"
     } else {
         messageErrorLogin()
     }
@@ -37,5 +37,6 @@ const messageErrorLogin = () => {
 
     emailLogin.value = ''
     passLogin.value = ''
+
 
 }
